c6914496e05adf0c736e43e4a598da11dde71577
lzl2040 <2040171586@qq.com>
2025-04-01 14:21:46 +0000

    update eval.sh

--- eval.sh
+++ eval.sh
@@ -1,18 +1,37 @@
 echo "eval orginal"
 CUDA_VISIBLE_DEVICES=0 accelerate launch --num_machines=1 --num_processes 1 --machine_rank 0 retrieval.py \
                                          --llava_llama3 \
-                                         --lora_path '/mnt/output_zuo/ZS-CIR/e5v-8b-4bit-ke/checkpoint-400' \
+                                         --lora_path '/mnt/output_zuo/ZS-CIR/e5v-8b-4bit-ke-2/checkpoint-400' \
                                          --file-path '/mnt/output_zuo/ZS-CIR/results' \
                                          --prompt_type 'org'
 echo "eval knowledge enhancments"
 CUDA_VISIBLE_DEVICES=0 accelerate launch --num_machines=1 --num_processes 1 --machine_rank 0 retrieval.py \
                                          --llava_llama3 \
-                                         --lora_path '/mnt/output_zuo/ZS-CIR/e5v-8b-4bit-ke/checkpoint-400' \
+                                         --lora_path '/mnt/output_zuo/ZS-CIR/e5v-8b-4bit-ke-2/checkpoint-400' \
                                          --file-path '/mnt/output_zuo/ZS-CIR/results' \
                                          --prompt_type 'ke'
 echo "eval chain of thought"
 CUDA_VISIBLE_DEVICES=0 accelerate launch --num_machines=1 --num_processes 1 --machine_rank 0 retrieval.py \
                                          --llava_llama3 \
-                                         --lora_path '/mnt/output_zuo/ZS-CIR/e5v-8b-4bit-ke/checkpoint-400' \
+                                         --lora_path '/mnt/output_zuo/ZS-CIR/e5v-8b-4bit-ke-2/checkpoint-400' \
+                                         --file-path '/mnt/output_zuo/ZS-CIR/results' \
+                                         --prompt_type 'cot'
+
+echo "eval orginal"
+CUDA_VISIBLE_DEVICES=0 accelerate launch --num_machines=1 --num_processes 1 --machine_rank 0 retrieval.py \
+                                         --llava_llama3 \
+                                         --lora_path '/mnt/output_zuo/ZS-CIR/e5v-8b-4bit-org/checkpoint-400' \
+                                         --file-path '/mnt/output_zuo/ZS-CIR/results' \
+                                         --prompt_type 'org'
+echo "eval knowledge enhancments"
+CUDA_VISIBLE_DEVICES=0 accelerate launch --num_machines=1 --num_processes 1 --machine_rank 0 retrieval.py \
+                                         --llava_llama3 \
+                                         --lora_path '/mnt/output_zuo/ZS-CIR/e5v-8b-4bit-org/checkpoint-400' \
+                                         --file-path '/mnt/output_zuo/ZS-CIR/results' \
+                                         --prompt_type 'ke'
+echo "eval chain of thought"
+CUDA_VISIBLE_DEVICES=0 accelerate launch --num_machines=1 --num_processes 1 --machine_rank 0 retrieval.py \
+                                         --llava_llama3 \
+                                         --lora_path '/mnt/output_zuo/ZS-CIR/e5v-8b-4bit-org/checkpoint-400' \
                                          --file-path '/mnt/output_zuo/ZS-CIR/results' \
                                          --prompt_type 'cot'
